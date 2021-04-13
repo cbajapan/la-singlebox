eded8e4fd69cc1317f5fe9a6932b9c8ed0dff9ba
rodbarnard <rod.barnard@outlook.com>
2021-04-13 17:09:12 +0200

    minor fixes

--- installer/cmd.sh
+++ installer/cmd.sh
@@ -7,14 +7,14 @@ if [[ ! -f /installer/cbala-install-status ]]; then
   # FAS install
   AS_INSTALLER=$(ls ./fas/*.jar)
   AS_OPTIONS=$(ls ./fas/*.advanced-install.properties)
-  echo -e "\e[1;33m Installing FAS \e[0m"
+  echo -e "\e[1;33mInstalling FAS: \e[0m"
   java -jar  $AS_INSTALLER -options $AS_OPTIONS
 
   # FCSDK install
     if [ $? -eq 0 ]; then
       SDK_INSTALLER=$(ls ./sdk/*.jar)
       SDK_OPTIONS=$(ls ./sdk/*.advanced-install.properties)
-      echo -e "\e[1;33m Installing FCSDK \e[0m"
+      echo -e "\e[1;33mInstalling FCSDK: \e[0m"
       java -jar $SDK_INSTALLER -options $SDK_OPTIONS
       else 
         exit
@@ -24,7 +24,7 @@ if [[ ! -f /installer/cbala-install-status ]]; then
     if [ $? -eq 0 ]; then
       LA_INSTALLER=$(ls ./liveassist/*.jar)
       LA_OPTIONS=$(ls ./liveassist/*.production.properties)
-      echo -e "\e[1;33m Installing Live Assist \e[0m"
+      echo -e "\e[1;33mInstalling Live Assist: \e[0m"
       java -jar $LA_INSTALLER -options $LA_OPTIONS
       else 
         exit
@@ -33,17 +33,17 @@ if [[ ! -f /installer/cbala-install-status ]]; then
   # Create check file for completed install
     if [ $? -eq 0 ]; then
       echo "completed" > /installer/cbala-install-status
-      echo -e "\e[1;32m Live Assist was installation completed! \e[0m"
+      echo -e "\e[1;32mLive Assist installation completed! \e[0m"
     else 
       exit
     fi
 
 else 
-  # Start FAS and Media Broker
+  # Start FAS and Media Broker if install check file is created
   [ -f /etc/init.d/fas ] && /etc/init.d/fas start || \
-  echo $'No fas service file found.\nDelete /installer/cbala-install-status to re-install'
+  echo -e "\e[1;31mNo fas service file found.\nDelete /installer/cbala-install-status to re-install. \e[1;31m"
   [ -f /etc/init.d/fusion_media_broker ] && /etc/init.d/fusion_media_broker start || \
-  echo $'No media broker service file found.\nDelete /installer/cbala-install-status  to re-install'                          
+  echo -e "\e[1;31mNo media broker service file found.\nDelete /installer/cbala-install-status to re-install. \e[1;31m"                         
 fi
 
 if (( $(ps -ef | grep -v grep | grep fas | wc -l) > 0 )) && \
@@ -55,11 +55,10 @@ if (( $(ps -ef | grep -v grep | grep fas | wc -l) > 0 )) && \
   \___||___//_/ \_\ |____||_| \_/ \___| /_/ \_\/__//__/|_|/__/ \__|
                                                                     
   "
-  echo -e "\e[1;32m Live Assist components are running! \e[0m" 
+  echo -e "\e[1;32mLive Assist components are running! \e[0m" 
   else 
-      echo -e "\e[1;31m There was a problem starting all CBA Live Assist components."
+      echo -e "\e[1;31mThere was a problem starting all CBA Live Assist components. \e[0m"
       exit
-
 fi
 
 exec /usr/sbin/init 
